91cfeef648b7bad81aafa62d63bade96a8df15f8
Adubedat <arthur.dubedat@gmail.com>
2021-03-29 16:35:38 +0200

    [feat] Add test launch script

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,11 @@ if [ $# -eq 0 ]
 
 elif [ $# -eq 1 ]
   then
+  if [ $1 == "test" ] || [ $1 == "coverage" ]; then
+    docker-compose -f docker-compose.test.yml build --build-arg environment="$1" && docker-compose up
+  else
     docker-compose build --build-arg environment="$1" && docker-compose up
+  fi
 
 elif [ $# -gt 1 ]
   then
@@ -14,6 +18,10 @@ elif [ $# -gt 1 ]
     shift
     for var in "$@"
     do
+      if [ $1 == "test" ] || [ $1 == "coverage" ]; then
+        docker-compose -f docker-compose.test.yml run -e ENVIRONMENT="$ENV" "$var"
+      else
         docker-compose run -e ENVIRONMENT="$ENV" "$var"
+      fi
     done
 fi
