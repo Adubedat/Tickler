3c2ea87f1fdc66efa4ff61e54d3025698800f5e9
Adubedat <arthur.dubedat@gmail.com>
2021-03-30 11:40:21 +0200

    [fix] remove useless file

--- scripts/createAdmin.sh
+++
@@ -1,6 +0,0 @@
-curl --header "Content-Type: application/json" --location \
---request POST 'http://localhost:3010/users' --data-raw '{
-    "username": "admin",
-    "password": "adminpwd",
-    "role": "admin"
-}'
