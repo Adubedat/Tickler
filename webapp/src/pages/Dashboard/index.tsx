3d522acf1532b584556492e961b8f66cd7988b81
Adubedat <arthur.dubedat@gmail.com>
2021-03-30 11:33:21 +0200

    [feat] empty Statistics page with render test

--- webapp/src/pages/Dashboard/index.tsx
+++ webapp/src/pages/Dashboard/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MainContainer, RowContainer, ColumnContainer } from './styles';
 import { withRouter } from 'react-router-dom'
 import Navbar from '../../components/Navbar';
 import LeftMenu from '../../components/LeftMenu/index';
 import TicketsList from '../../components/Tickets';
+import Statistics from '../../components/Statistics';
 
 export const Dashboard = () => {
+  const [selectedComponent, setSelectedComponent] = useState('tickets');
+
+  const displayComponent = () => {
+    if (selectedComponent === 'tickets') {
+      return (<TicketsList />);
+    }
+    else if (selectedComponent === 'statistics') {
+      return (<Statistics />);
+    }
+  }
+
   return (
     <MainContainer>
       <ColumnContainer>
         <Navbar />
         <RowContainer>
-          <LeftMenu />
-          <TicketsList />
+          <LeftMenu selectedComponent={selectedComponent} setSelectedComponent={setSelectedComponent}/>
+          {displayComponent()}
         </RowContainer>
       </ColumnContainer>
     </MainContainer>
